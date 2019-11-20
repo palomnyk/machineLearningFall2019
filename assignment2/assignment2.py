4b1f886cf6de8cfbfcec6e2f276442af9f65226a
Aaron_Y <amyerke@uncc.edu>
2019-11-19 19:04:05 -0500

    training and testing datasets created

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -80,43 +80,30 @@ class my_logistic_reg:
             dj = d_slope
         print('done with while')
         print(dj)
-        print(self.slope = np.zeros(n_features))
-        print(self.y_intercept = 0)
+        # print(self.slope)
+        # print(self.y_intercept = 0)
         
     def test(self):
         pass
 
-
-
-
-
 # --------------------------------------------------------------------------
 # create training and testingdatasets
 # --------------------------------------------------------------------------
-# diabetes = datasets.load_diabetes()
-# x = diabetes.data[:,2]
-# y = diabetes.target
-# n_samples = len(diabetes.target)
-# random_samples = np.random.randint(low = 0, high = n_samples, size = 20)
-# testing_x = np.asarray(x[random_samples])
-# training_x = np.asarray([x[i] for i in range(0, n_samples) if i not in random_samples])
-# testing_y = np.asarray(y[random_samples])
-# training_y = np.asarray([y[i] for i in range(0, n_samples) if i not in random_samples])
-
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns = iris['feature_names'])
 df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
-# df.to_csv('testing.tsv', sep='\t')
 df = df[df.species != "setosa"]
+df = df.reset_index(drop = True)
+df.to_csv('testing.tsv', sep='\t')
+#print(df.shape[0])
+testing_index = np.random.randint(low = 0, high = df.shape[0] - 1, size = 1 )[0]
+#print(testing_index)
+training_data = df.drop(testing_index, axis = 0)
+testing_data = df.iloc[testing_index]
 
-random_num = np.random.randint(low = 0, high = df.index - 1, size = df.index)
+#print(training_data.head)
 
-training_data = df[random_num]
-# testing_data = df[!random_num]
-
-print(training_data.head)
-
-print(df.head)
+# print(df.head)
 
 
 # --------------------------------------------------------------------------
@@ -126,101 +113,9 @@ print(df.head)
 
 
 
-
-# # --------------------------------------------------------------------------
-# # set up lm class
-# # --------------------------------------------------------------------------
-# class d_lm:
-#     def __init__(self, x, y, confidence = 0.95):
-#         self.confidence = confidence
-#         self.x = x
-#         self.y = y
-#         self.n = len(x)
-#         self.x_bar = np.mean(x)
-#         self.y_bar = np.mean(y)
-#         self.S_yx = np.sum((y - self.y_bar) * (x - self.x_bar))
-#         self.S_xx = np.sum((x - self.x_bar)**2)
-#         # ====== estimate beta_0 and beta_1 ======
-#         self.beta_1_hat = self.S_yx / self.S_xx # also equal to (np.cov(x, y))[0, 1] / np.var(x)
-#         self.beta_0_hat = self.y_bar - self.beta_1_hat * self.x_bar
-#         # ====== estimate sigma ======
-#         # residual
-#         self.y_hat = self.beta_0_hat + self.beta_1_hat * x
-#         self.r = y - self.y_hat
-#         self.sigma_hat = np.sqrt(sum(self.r**2) / (self.n-2))
-#         # ====== estimate sum of squares ======
-#         # total sum of squares
-#         self.SS_total = np.sum((y - self.y_bar)**2)
-#         # regression sum of squares
-#         self.SS_reg = np.sum((self.y_hat - self.y_bar)**2)
-#         # residual sum of squares
-#         self.SS_err = np.sum((y - self.y_hat)**2)
-#         # ====== estimate R2: coefficient of determination ======
-#         self.R2 = self.SS_reg / self.SS_total
-#         # ====== R2 = correlation_coefficient**2 ======
-#         self.correlation_coefficient = np.corrcoef(x, y)
-#         self.delta = self.correlation_coefficient[0, 1]**2 - self.R2
-#         # ====== estimate MS ======
-#         # sample variance
-#         self.MS_total = self.SS_total / (self.n-1)
-#         self.MS_reg = self.SS_reg / 1.0
-#         self.MS_err = self.SS_err / (self.n-2)
-#         # ====== estimate F statistic ======
-#         self.F = self.MS_reg / self.MS_err
-#         self.F_test_p_value = 1 - stats.f._cdf(self.F, dfn=1, dfd=self.n-2)
-#         # ====== beta_1_hat statistic ======
-#         self.beta_1_hat_var = self.sigma_hat**2 / ((self.n-1) * np.var(x))
-#         self.beta_1_hat_sd = np.sqrt(self.beta_1_hat_var)
-#         # confidence interval
-#         self.z = stats.t.ppf(q=0.025, df=self.n-2)
-#         self.beta_1_hat_CI_lower_bound = self.beta_1_hat - self.z * self.beta_1_hat_sd
-#         self.beta_1_hat_CI_upper_bound = self.beta_1_hat + self.z * self.beta_1_hat_sd
-#         # hypothesis tests for beta_1_hat
-#         # H0: beta_1 = 0
-#         # H1: beta_1 != 0
-#         self.beta_1_hat_t_statistic = self.beta_1_hat / self.beta_1_hat_sd
-#         self.beta_1_hat_t_test_p_value = 2 * (1 - stats.t.cdf(np.abs(self.beta_1_hat_t_statistic), df=self.n-2))
-#         # ====== beta_0_hat statistic ======
-#         self.beta_0_hat_var = self.beta_1_hat_var * np.sum(x**2) / self.n
-#         self.beta_0_hat_sd = np.sqrt(self.beta_0_hat_var)
-#         # confidence interval
-#         self.beta_0_hat_CI_lower_bound = self.beta_0_hat - self.z * self.beta_0_hat_sd
-#         self.beta_1_hat_CI_upper_bound = self.beta_0_hat + self.z * self.beta_0_hat_sd
-#         self.beta_0_hat_t_statistic = self.beta_0_hat / self.beta_0_hat_sd
-#         self.beta_0_hat_t_test_p_value = 2 * (1 - stats.t.cdf(np.abs(self.beta_0_hat_t_statistic), df=self.n-2))
-#         # confidence interval for the regression line
-#         self.sigma_i = 1.0/self.n * (1 + ((x - self.x_bar) / np.std(x))**2)
-#         self.y_hat_sd = self.sigma_hat * self.sigma_i
-#         self.y_hat_CI_lower_bound = self.y_hat - self.z * self.y_hat_sd
-#         self.y_hat_CI_upper_bound = self.y_hat + self.z * self.y_hat_sd
-
-#     def test_model_RMSE(self, test_x, test_y):
-#         pred_y_test = self.beta_0_hat + self.beta_1_hat * test_x
-#         pred_y_train = self.beta_0_hat + self.beta_1_hat * self.x
-#         train_rmse = sum(((self.y - pred_y_train)**2)/len(self.y))**(1/2)
-#         test_rmse = sum(((test_y - pred_y_test)**2)/len(test_y))**(1/2)
-#         fig = plt.figure()
-#         ax = fig.add_subplot(1, 1, 1)
-#         ax.scatter(self.x, self.y, label=f'training data, RMSE:{round(train_rmse,1)}', color='yellow', marker=marker_1, linewidth=line_width_1)
-#         ax.scatter(test_x, test_y, label=f'true vals of testing data, RMSE:{round(test_rmse,1)}', color='red', marker=marker_1, linewidth=line_width_1)
-#         ax.scatter(test_x, pred_y_test, color='green', label='predicted vals of testing data', linewidth=line_width_1)
-#         ax.plot(self.x, self.y_hat, color='blue', label='model from training data', linewidth=line_width_1)
-#         ax.set_xlabel('x')
-#         ax.set_ylabel('y')
-#         ax.set_title("Linear regression of diabetes data with d_lm class")
-#         ax.legend(loc='lower right', fontsize=9)
-#         fig.show()
-        
-
-# # --------------------------------------------------------------------------
-# # run homemade lm model
-# # --------------------------------------------------------------------------
-# my_lm = d_lm(training_x, training_y)
-# my_lm.test_model_RMSE(testing_x , testing_y)
-
-# # --------------------------------------------------------------------------
-# # run professional lm model
-# # --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# run professional lm model
+# --------------------------------------------------------------------------
 
 # training_x = training_x.reshape((len(training_x), 1))
 # testing_x = testing_x.reshape((len(testing_x), 1))
