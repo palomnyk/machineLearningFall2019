bf7d7f5b7bd8eb46bcb23c7d6d0b180210b84155
Aaron_Y <amyerke@uncc.edu>
2019-11-26 18:48:40 -0500

    new loss function, normalize training data

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -21,6 +21,7 @@ from sklearn import linear_model
 from sklearn import datasets
 # for dataset
 from sklearn.datasets import load_iris
+from sklearn import preprocessing
 
 # --------------------------------------------------------------------------
 # Some useful functions
@@ -31,18 +32,21 @@ def logit(p):
 def sigmoid(line):
     return 1/(1 + np.exp(-line))
 
-def loss_j(X,y, coef, intercept):
-    total_cost = 0
-    for row in range(X.shape[0]):
-#        print(f"coef.T: {coef.T}, np.array(X.iloc[row,]): {np.array(X.iloc[row,])}")
-        term = np.log(sigmoid((np.dot( coef.T, np.array(X.iloc[row,])))))
-        total_cost += y.iloc[row,] * term + (1-y.iloc[row,] * (term))
-    return - total_cost/X.shape[0]
+def loss_j(X,y, coef):
+#    total_cost = 0
+#    for row in range(X.shape[0]):
+##        print(f"coef.T: {coef.T}, np.array(X.iloc[row,]): {np.array(X.iloc[row,])}")
+#        term = np.log(sigmoid((np.dot( coef.T, np.array(X.iloc[row,])))))
+#        total_cost += y.iloc[row,] * term + (1-y.iloc[row,] * (term))
+#    return - total_cost/X.shape[0]
+    return 1/len(y) * (y @ np.log(sigmoid(X@coef)) - (1-y) @ np.log(1-sigmoid(X@coef)) )
+
+
 # --------------------------------------------------------------------------
 # set up log reg class
 # --------------------------------------------------------------------------
 class my_logistic_reg:
-    def __init__(self, lr = 0.001, n_iter = 1000, dj_stop = 0.00001):
+    def __init__(self, lr = 0.001, n_iter = 1000, dj_stop = 0.0000000001):
         self.slopes = None
         self.y_intercept = None
         self.lr = lr
@@ -61,41 +65,42 @@ class my_logistic_reg:
         
         print(f'{self.dj_stop} and {self.dj_stop} {len(self.cost_j)} {self.n_iter}')
 
-        # gradient descent
-        while all(d_slope >= self.dj_stop) and len(self.cost_j) <= self.n_iter:
+        # gradient descent   
+        while len(self.cost_j) < self.n_iter:#this is basically a for-loop that will go n iterations
             # approximate y with linear combination of slope and x, plus y_intercept
             linear_model = np.dot(my_x, self.slopes) + self.y_intercept
             # apply sigmoid function
             y_predicted = sigmoid(linear_model)
-            
-#            print(f"slopes: {self.slopes}, intercepts {self.y_intercept}")
-            print(list(zip(my_y, y_predicted)))
-            
-            self.cost_j.append(loss_j(my_x, my_y, self.slopes, self.y_intercept))
-
+            loss = loss_j(my_x, my_y, self.slopes)
+#            print(f"slopes: {self.slopes}, intercepts {self.y_intercept}")            
+            self.cost_j.append(loss)
             # compute gradients
-            d_slope = (1 / n_samples) * np.dot(my_x.T, (y_predicted - my_y))#this is the paritial deriviative
+            d_slope = (1 / n_samples) * np.sum( np.dot(my_x.T, (y_predicted - my_y)))#this is the paritial deriviative
             d_intercept = (1 / n_samples) * np.sum(y_predicted - my_y)
-            
-#            print(d_slope)
-#            print(d_intercept)
             # update parameters
             self.slopes -= self.lr * d_slope
             self.y_intercept -= self.lr * d_intercept
-            di = d_intercept
-#           print(d_slope)
-#        print(f'done with while iters:{len(self.cost_j)}')
-            
-        
-        print(self.slopes)
-        print(self.y_intercept)
-        print(len(self.cost_j))
-        print(self.cost_j)
-        
+            print(f"in iters {self.cost_j}")
+            if len(self.cost_j) == 0:
+                self.cost_j.append(loss)
+                print("in if")
+            elif loss - self.cost_j[-1] <= self.dj_stop:
+                self.cost_j.append(loss)
+                print(f'done with while iters:{len(self.cost_j)}')
+                print(self.slopes)
+                print(self.y_intercept)
+                print(len(self.cost_j))
+                print(self.cost_j)
+                break#get out of while loop
+
+                
     def test_model(self):
         pass
     
     def plot_cost(self):
+        pass
+    
+    def _grad_des(self):
         pass
 # --------------------------------------------------------------------------
 # create training and testingdatasets
@@ -110,6 +115,11 @@ df.to_csv('testing.tsv', sep='\t')
 #print(df.shape[0])
 
 df_x = df.iloc[:,list(range(df.shape[1]-1))]
+#normalize data to max value
+x = df_x.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+df_x = pd.DataFrame(x_scaled)
 print(f'df_x.shape: {df_x.shape}')
 df_y = df.iloc[:,df.shape[1]-1]
 print(f'df_y.shape: {df_y.shape}')
@@ -141,17 +151,14 @@ my_lr.fit_model(train_x, train_y)
 # --------------------------------------------------------------------------
 # run professional log reg model
 # --------------------------------------------------------------------------
-#print('pro_stuff')
-#skl_log = linear_model.LogisticRegression(solver="lbfgs")
-#skl_log.fit(X=train_x, y=train_y)
-#skl_pred = skl_log.predict(test_x)
-#print(f'sklearn log rediction: {skl_pred}')
-#print(f'Correct answer: {test_y}, {test_y == skl_pred}')
-#print(skl_log.coef_)
-#print(skl_log.intercept_)
-#        
-#my_row = np.array(train_x.iloc[1,])
-#
-#skl_log_cost = loss_j(train_x, train_y, skl_log.coef_, skl_log.intercept_)
+print('pro_stuff')
+skl_log = linear_model.LogisticRegression(solver="lbfgs")
+skl_log.fit(X=train_x, y=train_y)
+skl_pred = skl_log.predict(test_x)
+print(f'sklearn log rediction: {skl_pred}')
+print(f'Correct answer: {test_y}, {test_y == skl_pred}')
+print(skl_log.coef_)
+print(skl_log.intercept_)
+#skl_log_cost = loss_j(train_x, train_y, skl_log.coef_)
 #print(f'2(c) the total final cost J: {skl_log_cost}')
    
